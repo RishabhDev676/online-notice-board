03e0573bf1aa53d8a37a8916a8bbd1a16e47d4a9
RISHABH DEV <168200651+RishabhDev676@users.noreply.github.com>
2026-07-14 23:44:14 +0530

    security: move SMTP credentials to environment variables
    Updated email configuration to use environment variables.

--- noticeboard/settings.py
+++ noticeboard/settings.py
@@ -157,12 +157,12 @@ LOGOUT_REDIRECT_URL = '/admin/login/'
 # -------------------------
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'os.getenv("EMAIL_HOST_PASSWORD")'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'onlinenoticesubscription@gmail.com'
-EMAIL_HOST_PASSWORD = 'glwfhaimhsngqzyt'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
